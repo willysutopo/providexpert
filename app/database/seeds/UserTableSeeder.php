6f0a5146b777586136663f778a768180ba888511
Pradhana Indra <pradhana.indra@gmail.com>
2014-11-15 14:56:54 +0700

    Fix wrong seeder. Now set expert role to "Expert" as it should be.

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -21,21 +21,21 @@ class UserTableSeeder extends Seeder {
     		$expert1->email = 'ssugondho@example.com';
     		$expert1->password = Hash::make('123456');
     		$expert1->save();
-    		$expert1->roles()->attach($roleUser->id);
+    		$expert1->roles()->attach($roleExpert->id);
 
     		$expert2 = new User;
     		$expert2->id = 3001;
     		$expert2->email = 'tjien.ronny@example.com';
     		$expert2->password = Hash::make('123456');
     		$expert2->save();
-    		$expert2->roles()->attach($roleUser->id);
+    		$expert2->roles()->attach($roleExpert->id);
 
     		$expert3 = new User;
     		$expert3->id = 3002;
     		$expert3->email = 'andri.t@example.com';
     		$expert3->password = Hash::make('123456');
     		$expert3->save();
-    		$expert3->roles()->attach($roleUser->id);
+    		$expert3->roles()->attach($roleExpert->id);
     		
 
     }
