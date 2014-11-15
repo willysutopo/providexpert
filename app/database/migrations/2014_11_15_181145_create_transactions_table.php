96b545d153bd103c4bc17ea283ab08549a15f16f
Vicky <vicky@Vicky.s>
2014-11-15 18:18:19 +0700

    add braintree manager

---
+++ app/database/migrations/2014_11_15_181145_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('transactions', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->int('user_id');
+			$table->string('trx_id', 50);
+			$table->string('status', 100);
+			$table->decimal('amount', 13);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('transactions');
+	}
+
+}
